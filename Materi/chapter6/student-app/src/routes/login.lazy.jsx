f55fffa130043d22d3ce211a422fb31664dfb735
OneR7 <rhenojulius@gmail.com>
2024-10-25 23:02:24 +0700

    chapter5 - 4

--- Materi/chapter6/student-app/src/routes/login.lazy.jsx
+++ Materi/chapter6/student-app/src/routes/login.lazy.jsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -6,58 +7,88 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 export const Route = createLazyFileRoute("/login")({
-    component: Login,
+  component: Login,
 });
 
 function Login() {
-    return (
-    <Row className="mt-5">
-            <Col className="offset-md-3">
-                <Card className="text-center">
-                    <Card.Header>Login</Card.Header>
-                    <Card.Body>
-                        <Form>
-                            <Form.Group
-                                as={Row}
-                                className="mb-3"
-                                controlId="formPlaintextEmail"
-                            >
-                                <Form.Label column sm="2">
-                                    Email
-                                </Form.Label>
-                                <Col sm="10">
-                                    <Form.Control
-                                        type="email"
-                                        placeholder="Email"
-                                        required
-                                    />
-                                </Col>
-                            </Form.Group>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                            <Form.Group
-                                as={Row}
-                                className="mb-3"
-                                controlId="formPlaintextPassword"
-                            >
-                                <Form.Label column sm="2">
-                                    Password
-                                </Form.Label>
-                                <Col sm="10">
-                                    <Form.Control
-                                        type="password"
-                                        placeholder="Password"
-                                        required
-                                    />
-                                </Col>
-                            </Form.Group>
-                        </Form>
-                        <div className="d-grid gap-2">
-                            <Button variant="primary">Login</Button>
-                        </div>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col md={3}></Col>
-        </Row>
-    );
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    
+    const body = {
+      email,
+      password,
+    };
+
+    const response = await fetch("http://localhost:3000/auth/login", {
+      body: JSON.stringify(body),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    if (result.success) {
+      localStorage.setItem("token", result.data.token);
+      return;
+    }
+    alert(result.message);
+  };
+
+  return (
+    <Row className="mt-5">
+      <Col className="offset-md-3">
+        <Card className="text-center">
+          <Card.Header>Login</Card.Header>
+          <Card.Body>
+            <Form onSubmit={onSubmit}>
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId="email"
+              >
+                <Form.Label column sm="2">
+                  Email
+                </Form.Label>
+                <Col sm="10">
+                  <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Col>
+              </Form.Group>
+
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId="password"
+              >
+                <Form.Label column sm="2">
+                  Password
+                </Form.Label>
+                <Col sm="10">
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Col>
+              </Form.Group>
+              <div className="d-grid gap-2">
+                <Button type="submit" variant="primary">Login</Button>
+              </div>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={3}></Col>
+    </Row>
+  );
 }
--- Materi/chapter6/student-app/src/routes/register.lazy.jsx
+++ Materi/chapter6/student-app/src/routes/register.lazy.jsx
@@ -1,13 +1,151 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
-export const Route = createLazyFileRoute("/register")({
+export const Route = createLazyFileRoute('/register')({
     component: Register,
-});
+})
 
 function Register() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [profilePicture, setProfilePicture] = useState('')
+
+    const onSubmit = async (event) => {
+    event.preventDefault()
+
+    if (password != confirmPassword) {
+        alert('Password and password confirmation must be same!')
+    }
+
+    const formData = new FormData()
+    formData.append('name', name)
+    formData.append('email', email)
+    formData.append('password', password)
+    formData.append('profile_picture', profilePicture)
+
+    await fetch('http://localhost:3000/auth/register', {
+        body: formData,
+        method: 'POST',
+    })
+
+    const result = await response.json()
+    if (result.success) {
+        localStorage.setItem('token', result.data.token)
+        return
+    }
+
+        alert(result.message)
+    } 
+
     return (
-        <div className="p-2">
-            <h3>Register</h3>
-        </div>
-    );
+        <Row className="mt-5">
+            <Col className="offset-md-3">
+        <Card>
+            <Card.Header className="text-center">Register</Card.Header>
+            <Card.Body>
+                <Form onSubmit={onSubmit}>
+                    <Form.Group as={Row} className="mb-3" controlId="name">
+                        <Form.Label column sm={3}>
+                            Name
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Name"
+                                required
+                                value={name}
+                                onChange={(event) => {
+                                    setName(event.target.value)
+                                }}
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="email">
+                        <Form.Label column sm={3}>
+                            Email
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                required
+                                value={email}
+                                onChange={(event) => {
+                                    setEmail(event.target.value)
+                                }}
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="password">
+                        <Form.Label column sm={3}>
+                            Password
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                required
+                                value={password}
+                                onChange={(event) => {
+                                    setPassword(event.target.value)
+                                }}
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="confirmPassword">
+                        <Form.Label column sm={3}>
+                            Confirm Password
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirm Password"
+                                required
+                                value={confirmPassword}
+                                onChange={(event) => {
+                                    setConfirmPassword(event.target.value)
+                                }}
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="profilePicture">
+                        <Form.Label column sm={3}>
+                            Profile Picture
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="file"
+                                placeholder="Choose file"
+                                required
+                                onChange={(event) => {
+                                    setProfilePicture(event.target.files[0])
+                                }}
+                                accept=".jpg,.png"
+                            />
+                        </Col>
+                    </Form.Group>
+
+                    <div className="d-grid gap-2">
+                        <Button variant="primary" type="submit">
+                            Register
+                        </Button>
+                        </div>
+                    </Form>
+                </Card.Body>
+            </Card>
+            </Col>
+            <Col md={3}></Col>
+        </Row>
+    )
 }
